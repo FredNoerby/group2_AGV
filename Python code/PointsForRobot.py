3c5c94bff9b041c87b70e1515622c4751aa83b55
Alexander Kamp Nielsen <akampnielsen@gmail.com>
2017-11-07 14:45:52 +0100

    added the points for the vicon

---
+++ Python code/PointsForRobot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('learning_tf')
+import rospy
+import tf
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('listener')
+    
+	# Points for the for robot
+    point1 = tf.TransformBroadcaster()
+    point2 = tf.TransformBroadcaster()
+    point3 = tf.TransformBroadcaster() 
+    point4 = tf.TransformBroadcaster() 
+
+    rate = rospy.Rate(10.0)
+
+
+
+ 
+    while not rospy.is_shutdown():
+        point1.sendTransform((1.5, -1.0, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         rospy.Time.now(),
+                         "/point1",
+                         "/world")
+
+        point2.sendTransform((1.5, -0.5, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         rospy.Time.now(),
+                         "/point2",
+                         "/world")	
+
+        point3.sendTransform((1.5, 0.5, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         rospy.Time.now(),
+                         "/point3",
+                         "/world")	
+
+        point4.sendTransform((1.5, 1.0, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         rospy.Time.now(),
+                         "/point4",
+                         "/world")	
+
+        rate.sleep()
+
