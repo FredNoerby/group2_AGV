a9c8e3bbadd057554fbf444e5ee948d6194f693b
Frederik Nørby <frederiknoerby@outlook.dk>
2017-11-23 11:45:21 +0100

    Fixed some warnings in the product class

--- Python code/product.py
+++ Python code/product.py
@@ -1,6 +1,7 @@
 # Imports the convertProductsToParts library used to get the parts of an product type
 import convertProductsToParts
 
+
 class Product:
     """ Class to represent products to be made 
     
@@ -11,7 +12,7 @@ class Product:
         status (str): the current state of the product 
     """
 
-    def __init__(self, id, type, status="Waiting"):
+    def __init__(self, unique_id, type_of_product, status="Waiting"):
         """ Product init method
 
         Args:
@@ -19,12 +20,12 @@ class Product:
             type (str): the type of product (P1, P2, P3, or P4)
             status (Optional[str]): the current state of the product 
         """
-        self.id = id
-        self.type = type
+        self.id = unique_id
+        self.type = type_of_product
         self.status = status
         self.parts = convertProductsToParts.return_parts(type)
 
-# Will only run if this is the main file being runned
+# Will only run if this is the main file being run
 if __name__ == '__main__':
     # Creates an Product object with the ID of 2112 and the type of P2
     new_product = Product(2112, "P2")
