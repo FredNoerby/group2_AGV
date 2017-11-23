67b4451b8f2c05b20aab46c044ff4bc31fe8d7d0
BiancaT <bianca.dianat@gmail.com>
2017-11-23 17:24:34 +0100

    Quality control thingy

--- Python code/quality_control.py
+++ Python code/quality_control.py
@@ -1,7 +1,48 @@
+from product import Product
+
 class QualityControl:
     """ Class to represent quality control units
 
     Attributes:
         id (int): A unique number for identifying a quality control unit
     """
-    #TODO: EVERYTHING
+
+
+    def __init__(self, unique_id):
+        """ Quality control init method
+
+    Args:
+        unique_id (int): Initializes the product ID"""
+        self.id = unique_id
+
+
+    def pass_product(self, product):
+        """ Changes status of product to 'passed'
+
+        Args:
+             product (Product) : product to pass
+        """
+
+        product.status = "Product " + str(self.id) + " passed from QC "
+
+
+    def fail_product(self, product):
+        """ Changes status of product to 'failed'
+
+        Args:
+            product (Product) : product to fail
+        """
+
+        product.status = "Product "+ str(self.id) + " failed from QC "
+
+# Will only run if this is the main file being run
+if __name__ == '__main__':
+    #Creates a new Product with ID of 1 and type P1
+
+    new_p = QualityControl(63)
+    new_product = Product(1, "P1")
+
+    print("Product ID: " + str(new_p.id))
+    print("Status received: " + new_product.status)
+    new_p.pass_product(new_product)
+    print("Current status: " + new_product.status)
