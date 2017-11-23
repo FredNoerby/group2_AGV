94806cd9ce7549cb482a409b2def3bc48f200b65
Frederik Nørby <frederiknoerby@outlook.dk>
2017-11-23 14:16:13 +0100

    Created files for the quality control and location classes

---
+++ Python code/location.py
@@ -0,0 +1,9 @@
+class Location:
+    """ Class to represent locations in the map
+
+    Attributes:
+        id (int): A unique number for identifying a location
+        position (float[list]): x, y, & z values for the location
+        orientation (float[list]): quaternion representing rotation
+    """
+    #TODO: EVERYTHING
---
+++ Python code/quality_control.py
@@ -0,0 +1,7 @@
+class QualityControl:
+    """ Class to represent quality control units
+
+    Attributes:
+        id (int): A unique number for identifying a quality control unit
+    """
+    #TODO: EVERYTHING
