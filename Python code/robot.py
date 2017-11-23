00b2f238dd8eafde097e228584557ab6e4cee31a
Frederik Nørby <frederiknoerby@outlook.dk>
2017-11-23 10:50:54 +0100

    Fixed errors and warnings in robot class

--- Python code/robot.py
+++ Python code/robot.py
@@ -3,7 +3,7 @@ class Robot:
     
     Attributes:
         id (int): A unique number for identifying robots
-        storage_slots (str[list]): the current state of the product 
+        storage (str[list]): the current state of the product
     """
 
     def __init__(self, id):
@@ -14,7 +14,6 @@ class Robot:
         """
         self.id = id
         self.storage = []
-
 
     def add_part_to_storage(self, part):
         """ Adds a part to the robots storage
@@ -27,13 +26,13 @@ class Robot:
         if len(self.storage) < 2:
             # Adds the part to the robots storage
             self.storage.append(part)
-            return ("Added " + part + " to robot with id: " + str(self.id))
+            return "Added " + part + " to robot with id: " + str(self.id)
         else:
             # Returns message that robot is full
-            return("Robot with id: " + self.id + " is full")
+            return"Robot with id: " + str(self.id) + " is full"
 
 
-# Will only run if this is the main file being runned
+# Will only run if this is the main file being run
 if __name__ == '__main__':
     # Creates an Robot object with the ID of 99
     mc_turner = Robot(99)
