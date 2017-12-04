6d0bb04077f0b2c3ff91a9bce663c0ac6a823ac3
Alexander Kamp Nielsen <akampnielsen@gmail.com>
2017-12-04 13:51:27 +0100

    Made the talker for controling the moters, this is running on raspbarry

---
+++ Python code/talker2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int16MultiArray
+
+def talker2():
+
+	rospy.init_node('talker2', anonymous=True)    
+	pub = rospy.Publisher('toggle_led', Int16MultiArray, queue_size=10)
+
+	rate = rospy.Rate(10) # 10hz
+	while not rospy.is_shutdown():
+		data = Int16MultiArray()
+		data.data = [0, 0]   #% rospy.get_time()
+		#pub.data[1] = 100
+		rospy.loginfo(data)
+		pub.publish(data)
+		rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker2()
+    except rospy.ROSInterruptException:
+        pass
