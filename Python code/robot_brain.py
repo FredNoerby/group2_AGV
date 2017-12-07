ddbea9fa37122a9f51e4ff695a62c9987ea37154
Frederik Nørby <frederiknoerby@outlook.dk>
2017-12-07 12:52:29 +0100

    The robot_brain.py file now listens to cmd_vel and publishes it for the arduino controller to read

--- Python code/robot_brain.py
+++ Python code/robot_brain.py
@@ -2,6 +2,7 @@
 import roslib; roslib.load_manifest('group2_agv')
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Float16MultiArray
 
 
 def callback(msg):
@@ -9,10 +10,23 @@ def callback(msg):
     rospy.loginfo("Linear Components: [%f, %f, %f]" % (msg.linear.x, msg.linear.y, msg.linear.z))
     rospy.loginfo("Angular Components: [%f, %f, %f]" % (msg.angular.x, msg.angular.y, msg.angular.z))
 
+    left_speed = msg.linear.x * 1250 + msg.angular.z * -1250
+    right_speed = msg.linear.x * 1250 + msg.angular.z * 1250
+
+    pub = rospy.Publisher('motor_control', Float16MultiArray, queue_size=10)
+
+    rate = rospy.Rate(10)  # 10hz
+    data = Float16MultiArray()
+    data.data = [left_speed, right_speed]
+    rospy.loginfo(data)
+    pub.publish(data)
+    rate.sleep()
+
 
 def listener():
-    rospy.init_node('cmd_vel_listener')
+    rospy.init_node('robot_brain', anonymous=True)
     rospy.Subscriber("/cmd_vel", Twist, callback)
+
     rospy.spin()
 
 if __name__ == '__main__':
