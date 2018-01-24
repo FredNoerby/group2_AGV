dfddf517db35e1e02dd0366da89dff901d3bdc06
Frederik Nørby <frederiknoerby@outlook.dk>
2018-01-24 11:01:48 +0100

    Added a python script for mapping coordinates with the vicon flag

---
+++ Python code/flag_mapping.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+import tf
+import rospy
+
+
+if __name__ == '__main__':
+    rospy.init_node('flag_mapping')
+    # Creates a transform listener used for getting flag location
+    listener = tf.TransformListener()
+    trans, rot = []
+
+    # Enters loop that will run until script or ROS is shutdown 
+    while not rospy.is_shutdown():
+
+        # Gets the transform and rotation from the maps zero position to the flag
+        try:
+            (trans, rot) = listener.lookupTransform('/map', '/vicon/Flag/Flag', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+
+        if trans != trans && rot != rot:
+            print("Trans: ", end="")
+            print(trans) 
+            print("Rot: ", end="")
+            print(rot)
+        rospy.sleep(10)
